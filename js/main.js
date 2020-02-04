fa687dd05e382cf06cff61020dac1a320124b76f
Iftekhar Rahman <iftekharmail1@gmail.com>
2020-02-04 09:39:59 -0800

    done

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
 jQuery(document).ready(function($){
 
-    // $(".toggle").click(function(){
-    //     // $(this).removeClass("active");
-    //     $(".back").addClass("ative");
-    // });
-
-    
+    // toggle btn js
     $(".back").click(function(){
         $(this).toggleClass("active");
+    });
+    $(".dashboard-icon3").click(function(){
+        $(".dropdown-wrap").toggleClass("active");
     });
 
     $("#ip1").click(function(){
@@ -26,131 +24,32 @@ jQuery(document).ready(function($){
         $(".ip5").toggleClass("active");
     });
 
-    // var ipnno = $(".ip").addClass("active"));
 
-    // $(".back").click(function(){
-    //     $
-    // });
-    // $(".back").click(function(){
-    // });
-    
-
-    // $(".header-area").sticky({ topSpacing: 0 });
-
-    // $('select').niceSelect();
-
-    // homepage-slides
-	// $(".homepage-slides").owlCarousel({
-	// 	items: 1,
-	// 	loop: true,
-	// 	nav: true,
-	// 	dots: false,
-    //     autoplay: false,
-    //     navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"]
-	// });
-
-        // perfectScrollbar
-        // $(".nice-select .list").perfectScrollbar();
-
-    // $("#range_49").ionRangeSlider({
-    //     type: "single",
-    //     min: 150,
-    //     max: 250,
-    //     from: 200,
-    //     to: 250,
-    //     prefix: "£",
-    //     step: 1,
-    //     onFinish: function(data) {
-    //         maxValue = data.from;
-    //         alert("Your Value: " + maxValue);
-    //     }
-    // });
+    $(".menu-btn").click(function(){
+        $(".sidebar").addClass("active");
+    });
+    $(".close-btn").click(function(){
+        $(".sidebar").removeClass("active");
+    });
 
 
-    // $(".logo-carousel-wrap").owlCarousel({
-    //     items: 5,
-    //     loop: true,
-    //     nav: false,
-    //     dots: false,
-    //     autoplay: false,
-    //     margin: 15,
-    //     navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
-    //     responsive: {
-    //         0: {
-    //             items: 2,
-    //             margin: 15,
-    //             nav: false
-    //         },
-    //         580: {
-    //             items: 3,
-    //             margin: 3,
-    //             nav: false
-    //         },
-    //         720: {
-    //             items: 4,
-    //             margin: 3,
-    //             nav: false
-    //         },
-    //         960: {
-    //             items: 5
-    //         },
-    //         1140: {
-    //             items: 6
-    //         }
-
-    //     }
-    // });
-
-    // $(".feature-carousel-wrap").owlCarousel({
-    //     items: 4,
-    //     loop: true,
-    //     nav: true,
-    //     dots: true,
-    //     autoplay: false,
-    //     margin: 15,
-    //     navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
-    //     responsive: {
-    //         0: {
-    //             items: 1,
-    //             margin: 0,
-    //             nav: false
-    //         },
-    //         481: {
-    //             items: 2,
-    //             margin: 15,
-    //             nav: false
-    //         },
-    //         720: {
-    //             items: 3,
-    //             margin: 15,
-    //             nav: false
-    //         },
-    //         960: {
-    //             items: 3,
-    //             margin: 25,
-    //         },
-    //         1140: {
-    //             items: 4
-    //         }
-
-    //     }
-    // });
-
-
-
-
-	// $(".bodytype-carousel-wrap").magnificPopup({
-	// 	type: 'image',
-    //     gallery: {
-    //         enabled: true
-    //     }
-	// });
-
-
-
-
-    
-
+    if (navigator.userAgent.toLowerCase().indexOf("chrome") >= 0) {
+        $('input, select').on('change focus', function (e) {
+            setTimeout(function () {
+                $.each(
+                    document.querySelectorAll('*:-webkit-autofill'),
+                    function () {
+                        var clone = $(this).clone(true, true);
+                        $(this).after(clone).remove();
+                        updateActions();
+                    })
+            }, 300)
+        }).change();
+    }
+    // var updateActions = function(){
+    //     $(":-webkit-autofill,:-webkit-autofill:hover,:-webkit-autofill:focus,:-webkit-autofill:active ").css("background:","transparent");
+    // };// method for update input actions
+    // updateActions(); // start on load and on rebuild
     
 
 });
